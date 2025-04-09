3de49b18f8faf8dad95aa2020b6f8709dc7d64b1
Soutif Thomas <th.soutif@gmail.com>
2025-04-09 10:49:37 +0200

    use t3 small for cluster

--- eks.tf
+++ eks.tf
@@ -24,7 +24,7 @@ resource "aws_eks_node_group" "worker-node-group" {
   node_group_name = "devops-workernodes"
   node_role_arn  = aws_iam_role.workernodes.arn
    subnet_ids = [aws_subnet.devops1.id, aws_subnet.devops2.id]
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small"]
  
   scaling_config {
    desired_size = 1
